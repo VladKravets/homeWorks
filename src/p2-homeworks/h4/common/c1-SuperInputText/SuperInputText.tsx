90f5155bade667138992b01574f9d9f843a9f8c7
Vlad Kravets <hazardvlad19@gmail.com>
2022-03-01 15:19:35 +0300

    'hw4'

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -42,7 +42,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
-    const finalInputClassName = `${s.errorInput} ${className ? s.superInput : ''}}` // need to fix with (?:) and s.superInput
+    const finalInputClassName = ` ${s.errorInput} ${className ? s.superInput : ''}}` // need to fix with (?:) and s.superInput
 
     return (
         <>
