c9a9606f2ba24349180626911294028ef55920af
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-11 13:06:00 +0300

    fixed reducer

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,20 +1,30 @@
-const initState: boolean = false
+export type StartLoadingType = {
+    type: 'STARTED-LOADING',
+    payload: {
+        value: boolean,
+    },
+}
+const initState = {
+    value: false,
+}
 
-export const loadingReducer = (state = initState, action: loadingAC): boolean => {
+
+export const loadingReducer = (state = initState, action: StartLoadingType): typeof initState => {
     switch (action.type) {
-        case "STARTED-LOADING":
-        {
-            return action.payload.value
+        case "STARTED-LOADING": {
+            return {
+                ...state,
+                value: action.payload.value
+            }
         }
-        default: return state
+        default:
+            return state
     }
 }
 type loadingAC = ReturnType<typeof loadingAC>
 export const loadingAC = (value: boolean) => {
-    return  {
+    return {
         type: "STARTED-LOADING",
-        payload:{
-            value
-        }
+        payload: { value },
     } as const
 }
