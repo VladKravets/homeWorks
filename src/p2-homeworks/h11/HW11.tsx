d3f5a8208830963b52fc9ba6dfd16ef65a1ea6a6
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-16 14:46:41 +0300

    completed task 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
-
+import s from './HW11.module.css'
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState(19)
+    const [value2, setValue2] = useState(78)
     const onChangeRange = (value: number | number[]) => {
         if (Array.isArray(value)) {
             setValue1(value[0])
@@ -14,10 +14,10 @@ function HW11() {
     return (
         <div>
             <hr/>
-            homeworks 11
+            <div className={s.title}>Homework 11</div>
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.range}>
                 <span>{value1}</span>
                 <SuperRange
                     value={value1}
@@ -27,13 +27,11 @@ function HW11() {
             </div>
 
             <div>
-                <span>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
                     value={[value1, value2]}
                     onChangeRange={onChangeRange}
                 />
-                <span>{value2}</span>
             </div>
 
             <hr/>
