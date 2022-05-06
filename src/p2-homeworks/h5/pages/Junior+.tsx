e7bab77c6072b2f5c1dbca281f20754dd80513d1
Vlad Kravets <hazardvlad19@gmail.com>
2022-05-06 12:21:41 +0300

    hw13

--- src/p2-homeworks/h5/pages/Junior+.tsx
+++ src/p2-homeworks/h5/pages/Junior+.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HW13 from "../../HW13/HW13";
 
 const JuniorPlus = () => {
     return (
         <div>
-           {/*<HW13/>*/}
+           <HW13/>
            {/*<HW14/>*/}
            {/*<HW15/>*/}
            {/*<HW16/>*/}
